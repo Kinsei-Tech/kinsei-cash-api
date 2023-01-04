e2b612df473c4b68ba3a9ed372ae3c6846943e36
richzago <rich.czago@gmail.com>
2023-01-04 15:08:12 -0300

    fix: bug in id

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -5,12 +5,16 @@ from .models import Transaction
 
 class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
+        id = serializers.UUIDField(read_only=True)
         model = Transaction
-        fields = ["name", "type", "date", "description", "value"]
-
-        read_only_fields = {
+        fields = [
+            "name",
+            "type",
+            "date",
+            "description",
+            "value",
             "id",
-        }
+        ]
 
     def create(self, validated_data):
         return Transaction.objects.create(**validated_data)
