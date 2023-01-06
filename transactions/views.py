cc05770e7e2286bf65ca23a9810201e03c4b52d6
richzago <rich.czago@gmail.com>
2023-01-06 19:27:59 -0300

    feat: overwrite perform create

--- transactions/views.py
+++ transactions/views.py
@@ -6,6 +6,7 @@ import json
 import ipdb
 from rest_framework import generics
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Transaction
 from .serializers import TransactionSerializer
 
@@ -20,19 +21,18 @@ def csv_data_handling(csv):
     for transaction in csv:
         transaction_str = str(transaction).lower().replace("'", '"')
         transaction_dict = json.loads(transaction_str)
-        del transaction_dict['identificador']
-        transaction_dict['value'] = transaction_dict.pop('valor')
-        transaction_dict['value'] = float(transaction_dict['value'])
-        transaction_dict['category'] = 'other'
-        if transaction_dict['value'] < 0:
-            transaction_dict['type'] = 'cashout'
+        del transaction_dict["identificador"]
+        transaction_dict["value"] = transaction_dict.pop("valor")
+        transaction_dict["value"] = float(transaction_dict["value"])
+        transaction_dict["category"] = "other"
+        if transaction_dict["value"] < 0:
+            transaction_dict["type"] = "cashout"
         else:
-            transaction_dict['type'] = 'cashin'
-        transaction_dict['date'] = transaction_dict.pop('data')
-        transaction_dict['description'] = transaction_dict.pop(
-            'descrição')
-        name_Find = transaction_dict['description'].find('-')
-        transaction_dict['name'] = transaction_dict['description'][0:name_Find]
+            transaction_dict["type"] = "cashin"
+        transaction_dict["date"] = transaction_dict.pop("data")
+        transaction_dict["description"] = transaction_dict.pop("descrição")
+        name_Find = transaction_dict["description"].find("-")
+        transaction_dict["name"] = transaction_dict["description"][0:name_Find]
 
         new_data_csv.append(transaction_dict)
     return new_data_csv
@@ -42,34 +42,35 @@ class ExcelAutoView(APIView):
     authentication_classes = [JWTAuthentication]
 
     def post(self, request):
-        doc_request = request.FILES['file']
-        file = doc_request.read().decode('utf-8')
+        doc_request = request.FILES["file"]
+        file = doc_request.read().decode("utf-8")
         reader = csv.DictReader(io.StringIO(file))
         data_csv = [line for line in reader]
         new_data_csv = csv_data_handling(data_csv)
 
         for data in new_data_csv:
-            category_value = data['category']
+            category_value = data["category"]
             category = Category.objects.get_or_create(name=category_value)[0]
             serializer = TransactionSerializer(data=data)
             serializer.is_valid(raise_exception=True)
             serializer.save(category=category)
 
-        return Response({'msg': 'extract successfully added'}, status.HTTP_201_CREATED)
+        return Response({"msg": "extract successfully added"}, status.HTTP_201_CREATED)
 
         # return Response(new_data_csv)
 
 
 class TransactionView(generics.ListCreateAPIView):
     authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     serializer_class = TransactionSerializer
     queryset = Transaction.objects.all()
 
     def perform_create(self, serializer):
-        category_value = self.request.data['category']
+        category_value = self.request.data["category"]
         category = Category.objects.get_or_create(name=category_value)[0]
-        serializer.save(category=category)
+        serializer.save(category=category, user=self.request.user)
 
 
 class TransactionDetailView(generics.RetrieveUpdateDestroyAPIView):
@@ -77,3 +78,13 @@ class TransactionDetailView(generics.RetrieveUpdateDestroyAPIView):
 
     serializer_class = TransactionSerializer
     queryset = Transaction.objects.all()
+
+    def perform_update(self, serializer):
+        ipdb.set_trace()
+        if self.request.data["category"]:
+            # category = Category.objects.get_or_create(self.request.data["category"], user=self.request.user)[0]
+            category = Category.objects.get_or_create(
+                name=self.request.data["category"]
+            )[0]
+            serializer.save(category=category)
+        serializer.save()
