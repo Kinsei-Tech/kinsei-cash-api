36c56e701e99729cd5a6e982003d8adfb8cce8c2
Laís Martins <silveiramartinslais@gmail.com>
2023-01-07 15:01:59 -0300

    fix: user register view updated, working 100%

--- tests/users/test_register_view.py
+++ tests/users/test_register_view.py
@@ -3,6 +3,7 @@ from rest_framework.views import status
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
 
+import uuid
 
 User: AbstractUser = get_user_model()
 
@@ -19,10 +20,9 @@ class UserRegistrationViewTest(APITestCase):
 
         resulted_data: dict = response.json()
         expected_fields = {
+            "username",
             "email",
             "password",
-            "total_balance",
-            "name",
         }
         returned_fields = set(resulted_data.keys())
         msg = (
@@ -42,10 +42,10 @@ class UserRegistrationViewTest(APITestCase):
     def test_user_creation_success(self):
         register_data = {
             "email": "lais_bomtempo@mail.com",
-            "name": "Laís Bomtempo",
+            "username": "laissm",
             "password": "1234",
-            "total_balance": 5000,
-            "goal_balance": 1000,
+            "total_balance": "5000.00",
+            "goal_balance": "1000.00",
         }
 
         response = self.client.post(self.BASE_URL, data=register_data, format="json")
@@ -53,12 +53,12 @@ class UserRegistrationViewTest(APITestCase):
         added_user = User.objects.last()
 
         expected_data = {
-            "id": added_user.pk,
+            "id": str(added_user.id),
             "email": "lais_bomtempo@mail.com",
-            "name": "Laís Bomtempo",
-            "total_balance": 5000,
-            "goal_balance": 1000,
-            "current_balance": 5000,
+            "username": "laissm",
+            "total_balance": "5000.00",
+            "goal_balance": "1000.00",
+            "current_balance": "5000.00",
             "is_active": True,
             "is_healthy": True,
         }
@@ -80,7 +80,7 @@ class UserRegistrationViewTest(APITestCase):
     def test_non_unique_email_user_creation(self):
         register_data = {
             "email": "lais_bomtempo@mail.com",
-            "name": "Laís Bomtempo",
+            "username": "laissm",
             "password": "1234",
             "total_balance": 5000,
             "goal_balance": 1000,
@@ -90,16 +90,22 @@ class UserRegistrationViewTest(APITestCase):
         response = self.client.post(self.BASE_URL, data=register_data, format="json")
 
         returned_data = response.json()
-        expected_fields = {"email"}
+        expected_fields = {"email", "username"}
         returned_fields = set(returned_data.keys())
         msg = f"Erro em POST rota {self.BASE_URL}. Fields esperados: {expected_fields}. Fields recebidos: {returned_fields}"
         self.assertSetEqual(expected_fields, returned_fields, msg)
 
         returned_email_message = returned_data["email"][0]
-        expected_email_message = "This field must be unique"
+        expected_email_message = "user with this email already exists."
 
         msg = f"Esperado mensagem de erro: {expected_email_message}. Recebido: {returned_email_message}"
         self.assertEqual(expected_email_message, returned_email_message, msg)
+
+        returned_username_message = returned_data["username"][0]
+        expected_username_message = "user with this username already exists."
+
+        msg = f"Esperado mensagem de erro: {expected_username_message}. Recebido: {returned_username_message}"
+        self.assertEqual(expected_username_message, returned_username_message, msg)
 
         expected_status_code = status.HTTP_400_BAD_REQUEST
         returned_status_code = response.status_code
