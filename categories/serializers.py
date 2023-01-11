bffceb0a00e2141e03c51280438860cb6b67f5f0
richzago <rich.czago@gmail.com>
2023-01-11 19:43:59 -0300

    feat: added extend schema serializer to documentation

--- categories/serializers.py
+++ categories/serializers.py
@@ -1,12 +1,41 @@
 from rest_framework import serializers
 import ipdb
-
 from .models import Category
 from transactions.models import Transaction
+from drf_spectacular.utils import extend_schema_serializer
+from drf_spectacular.utils import OpenApiExample
 
-from transactions.models import Transaction
 
-
+@extend_schema_serializer(
+    examples=[
+        OpenApiExample(
+            "List Category Example",
+            value={
+                "id": "f7d17ccf-d44c-4765-826d-036a46fd2d9b",
+                "name": "food",
+                "limit": "200.00",
+                "categories_transactions": [
+                    {
+                        "id": "1decf24d-c8b8-4ba7-bd51-b8b54b64a011",
+                        "name": "Test",
+                        "type": "cashout",
+                        "date": "2023-01-11T20:00:10.300537Z",
+                        "description": "My description",
+                        "value": "10.00",
+                        "user": "359f99b2-2c14-4666-b275-ca1f5dc3a504",
+                        "category": "f7d27ccb-644c-4765-826d-036a46fd2d9b",
+                    }
+                ],
+                "money_available_category": 3000.0,
+                "is_healthy": True,
+            },
+        ),
+        OpenApiExample(
+            "Update Category Example",
+            value={"value": "Put the value here"},
+        ),
+    ]
+)
 class CategorySerializer(serializers.ModelSerializer):
 
     is_healthy = serializers.SerializerMethodField()
